22b077bc2c4c23aefd5b67566cc99734ae3c24a4
matbroughty <mat@broughty.com>
2020-04-10 08:41:28 +0100

    RMurder capital - tidied up generator

--- tools/src/main/kotlin/com/timstwitterlisteningparty/tools/shell/CsvToHtmlCommand.kt
+++ tools/src/main/kotlin/com/timstwitterlisteningparty/tools/shell/CsvToHtmlCommand.kt
@@ -5,7 +5,6 @@ import com.timstwitterlisteningparty.tools.parser.TimeSlot
 import org.slf4j.LoggerFactory
 import org.springframework.shell.standard.ShellComponent
 import org.springframework.shell.standard.ShellMethod
-import org.springframework.shell.standard.ShellOption
 import java.io.File
 import java.io.FileReader
 import java.time.DayOfWeek
@@ -25,19 +24,16 @@ class CsvToHtmlCommand {
 
 
   @ShellMethod("Produces the completed-time-slots.html, date-tbd-time-slots.html and the upcoming-time-slots.html files from a csv file - defaults to using time-slots-data.csv")
-  fun html(@ShellOption("-b", "--bootstrap", defaultValue = "true") bootStrap: String
-  ): String {
-    return "The html file was created ${createFiles(bootStrap.toBoolean())}"
+  fun html(): String {
+    return "The html file was created ${createFiles()}"
   }
 
-  fun createFiles(bootStrap: Boolean): String {
-    logger.debug("File bootstrap {}",bootStrap)
+  fun createFiles(): String {
     // default file to read
     val fileName = "time-slot-data.csv"
     val beans: List<TimeSlot> = CsvToBeanBuilder<TimeSlot>(FileReader(fileName))
       .withType(TimeSlot::class.java).withIgnoreEmptyLine(true).build().parse()
     beans.forEach { logger.debug("Read in Bean {}", it) }
-
     val tbd = beans.stream()
       .filter { it.isoDate.year == 1970 }.collect(Collectors.toList())
     val completed = beans.stream()
@@ -49,25 +45,20 @@ class CsvToHtmlCommand {
     tbd.forEach { logger.info("Dates to be confirmed {}", it) }
     completed.forEach { logger.info("Completed listening {}", it) }
     upcoming.forEach { logger.info("Upcoming listening {}", it) }
-    val upcomingHtml = buildTable(upcoming, false, tbd = false, bootStrap = bootStrap)
+    val upcomingHtml = buildTable(upcoming, false, tbd = false)
     File("upcoming-time-slots.html").writeText(upcomingHtml)
-    val dateTbdHtml = buildTable(tbd, false, tbd = true, bootStrap = bootStrap)
+    val dateTbdHtml = buildTable(tbd, false, tbd = true)
     File("date-tbd-time-slots.html").writeText(dateTbdHtml)
-    val completedHtml = buildTable(completed, true, tbd = false, bootStrap = bootStrap)
+    val completedHtml = buildTable(completed, true, tbd = false)
     File("completed-time-slots.html").writeText(completedHtml)
     logger.info("Upcoming\n {} \nDateTbd \n{} \ncompleted\n {}", upcomingHtml, dateTbdHtml, completedHtml)
     return upcomingHtml.plus(dateTbdHtml).plus(completed)
 
   }
 
-  private fun buildTable(slots: List<TimeSlot>, completed: Boolean, tbd: Boolean, bootStrap: Boolean = false): String {
+  private fun buildTable(slots: List<TimeSlot>, completed: Boolean, tbd: Boolean): String {
     var section = "<section class=\"post\">\n"
-    val sortedSlots =
-      when {
-          tbd -> slots.sortedBy { it.band }
-          completed -> slots.sortedBy{ it.isoDate }
-          else -> slots.sortedBy { it.isoDate }
-      }
+    val sortedSlots = if (tbd) slots.sortedBy { it.band } else slots.sortedBy { it.isoDate }
     logger.debug("Sorted slots for completed {} and tbd {}", completed, tbd)
     sortedSlots.forEach { logger.debug("Time listening {}", it) }
     if (!tbd) {
@@ -83,44 +74,36 @@ class CsvToHtmlCommand {
       }
       map.keys.sortedBy { it }.forEach { logger.debug("key is {}", it) }
       sortedSlots.forEach { map[it.isoDate.toLocalDate().with(TemporalAdjusters.previousOrSame(DayOfWeek.MONDAY))]?.add(it) }
-      map.keys.stream().sorted(if(completed) reverseOrder() else naturalOrder())
-        .map { pureTable(it, map[it], completed, bootStrap) }
+      map.keys.stream().sorted(if (completed) reverseOrder() else naturalOrder())
+        .map { pureTable(it, map[it], completed) }
         .collect(Collectors.toList())
         .forEach { section = section.plus(it) }
     } else {
-      section = section.plus(pureTable(null, sortedSlots, completed, bootStrap))
+      section = section.plus(pureTable(null, sortedSlots, completed))
     }
     return section.plus("\n</section>")
   }
 
 
-  private fun pureTable(monday: LocalDate?, rows: List<TimeSlot>?, completed: Boolean, bootStrap: Boolean = false): String {
+  private fun pureTable(monday: LocalDate?, rows: List<TimeSlot>?, completed: Boolean): String {
 
     var h2Value = "Upcoming Events - Dates to be confirmed"
-
     if (monday != null) {
       val df = DateTimeFormatter.ofPattern("EEEE, MMMM d")
-      h2Value = if (completed || previousWeek(monday)) "Week that commenced ${monday.format(df)}" else "Week commencing ${monday.format(df)}"
+      h2Value = if (completed || previousWeek(monday))
+        "Week that commenced ${monday.format(df)}"
+      else "Week commencing ${monday.format(df)}"
     }
-
-
-    val pureHeader = "\n          <header class=\"post-header\">\n" +
-      "            <h2 class=\"post-title\">$h2Value</h2>\n" +
-      "\n"
-
 
     var icon = "<i class=\"fas fa-calendar-day\"></i>"
-    if(completed){
+    if (completed) {
       icon = "<i class=\"fas fa-calendar-check\"></i>"
     }
-
-    val bstrapHeader = "  <div class=\"card bg-light mb-2 border-dark \" style=\"max-width\">\n" +
-      "    <div class=\"card-header\">$icon $h2Value</div>\n" +
-      "    <div class=\"card-body p-0\">"
-
-
     var htmlTable =
-      "            <div class=\"scroll-table\">\n" +
+      "  <div class=\"card bg-light mb-2 border-dark \" style=\"max-width\">\n" +
+        "    <div class=\"card-header\">$icon $h2Value</div>\n" +
+        "    <div class=\"card-body p-0\">" +
+        "            <div class=\"scroll-table\">\n" +
         "              <table width=\"100%\" class=\"pure-table\">\n" +
         "                <thead>\n" +
         "                <tr>\n" +
@@ -133,32 +116,16 @@ class CsvToHtmlCommand {
         "                </thead>\n" +
         "                <tbody>"
 
-    htmlTable = if (bootStrap) {
-      bstrapHeader.plus(htmlTable)
-    } else {
-      pureHeader.plus(htmlTable)
-    }
-
-
+    // add each row
     rows?.forEach { htmlTable = htmlTable.plus(it.buildHtmlRow()) }
-
-    htmlTable = htmlTable.plus("\n                </tbody>\n" +
-      "              </table>\n")
-
-
-    if (bootStrap) {
-      htmlTable = htmlTable.plus("   </div></div></div>\n")
-    } else {
-      htmlTable = htmlTable.plus("   </div></header>\n")
-    }
-
-    return htmlTable
-
+    // close table and divs
+    return htmlTable.plus("\n                </tbody>\n" +
+      "              </table>\n   </div></div></div>\n")
   }
 
 
   /**
-   * If this week or a week in the future return true else false
+   * If this week or a week in the future return false else true
    */
   private fun previousWeek(monday: LocalDate): Boolean {
     if (monday.isEqual(LocalDate.now())) {
