d1bbe3d7b03cad2d734b7e8a4697e4b6fd2ca21f
matbroughty <mat@broughty.com>
2020-05-04 08:11:28 +0100

    Included pages in cache invalidation and everything in snippets and sub folders

--- scripts/invalidate-cache-index-snippets-aws.bat
+++ scripts/invalidate-cache-index-snippets-aws.bat
@@ -1,3 +1,3 @@
 REM creates an invalidation on the cloud front cache to ensure the latest changes in the s3 bucket are visible
-aws cloudfront create-invalidation --distribution-id E3VMVDBEUWOSL5 --paths "/index.html" "/snippets/*"
+aws cloudfront create-invalidation --distribution-id E3VMVDBEUWOSL5 --paths "/index.html" "/snippets*"
 pause
