ced19b82a50629970f5830ee899252da2c99061e
matbroughton <mat@broughty.com>
2020-11-21 09:19:17 +0000

    collection_542.html 20th Nov

--- scripts/update-lambda-functions.sh
+++ scripts/update-lambda-functions.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-aws lambda update-function-code --function-name html-generator --s3-bucket timstwitterlisteningparty.com --s3-key tools-1.0.0-all.jar
-aws lambda update-function-code --function-name ScheduledHtmlGenerator --s3-bucket timstwitterlisteningparty.com --s3-key tools-1.0.0-all.jar
-aws lambda update-function-code --function-name ScheduledReplayTweeter --s3-bucket timstwitterlisteningparty.com --s3-key tools-1.0.0-all.jar
-aws lambda update-function-code --function-name TimeSlotFileEnrichHandler --s3-bucket timstwitterlisteningparty.com --s3-key tools-1.0.0-all.jar
-aws lambda update-function-code --function-name ScheduledAnniversaryTweeter --s3-bucket timstwitterlisteningparty.com --s3-key tools-1.0.0-all.jar
+aws lambda update-function-code --function-name html-generator --s3-bucket timstwitterlisteningparty.com --s3-key tools-1.0.0-all.jar;
+aws lambda update-function-code --function-name ScheduledHtmlGenerator --s3-bucket timstwitterlisteningparty.com --s3-key tools-1.0.0-all.jar;
+aws lambda update-function-code --function-name ScheduledReplayTweeter --s3-bucket timstwitterlisteningparty.com --s3-key tools-1.0.0-all.jar;
+aws lambda update-function-code --function-name TimeSlotFileEnrichHandler --s3-bucket timstwitterlisteningparty.com --s3-key tools-1.0.0-all.jar;
+aws lambda update-function-code --function-name ScheduledAnniversaryTweeter --s3-bucket timstwitterlisteningparty.com --s3-key tools-1.0.0-all.jar;
