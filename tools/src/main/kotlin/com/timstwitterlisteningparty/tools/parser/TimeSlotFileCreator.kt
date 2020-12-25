136e3a0d50df0933a8dee39328c137fa20f4eb50
matbroughton <mat@broughty.com>
2020-12-25 09:50:27 +0000

    collections 24th Dec

--- tools/src/main/kotlin/com/timstwitterlisteningparty/tools/parser/TimeSlotFileCreator.kt
+++ tools/src/main/kotlin/com/timstwitterlisteningparty/tools/parser/TimeSlotFileCreator.kt
@@ -60,7 +60,7 @@ class TimeSlotFileCreator : HtmlFileCreator {
     val allOneTableFile = File("snippets/all-time-slots.html")
     val wallHtml = buildWallHtml(completed, upcoming)
     val wallFile = File("snippets/wall.html")
-    val wallFullSizeHtml = buildWallHtml(completedFullSizeWall, upcoming, true)
+    val wallFullSizeHtml = buildWallHtml(completedFullSizeWall, upcoming, true, split = 27)
     val wallFullSize = File("pages/timswall.html")
 
     // if called from Lambda we can't write to the file
