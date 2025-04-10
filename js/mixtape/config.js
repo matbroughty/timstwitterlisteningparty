a1ac1fa3d06e305de68d599e74f4cce49bdb166a
ajbrindle <andrew.brindle@gmail.com>
2025-04-10 23:55:21 +0100

    Fix redirect URI

--- js/mixtape/config.js
+++ js/mixtape/config.js
@@ -1,4 +1,4 @@
-var REDIRECT_URI='https://www.timstwitterlisteningparyt.com/pages/mixtape.html';
+var REDIRECT_URI='https://www.timstwitterlisteningpary.com/pages/mixtape.html';
 var PLAYLIST_CREATE_URI='https://timstwitterlisteningparty.nw.r.appspot.com/playlist.php';
 const SPOTIFY_CLIENT_ID='cb722e55e2474dfbb88ae28f962da9cb';
 
